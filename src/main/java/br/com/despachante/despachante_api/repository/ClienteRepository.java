0d918d66e1b95161440c0e09ce68ee9e81c40e51
Tassia Monteiro <tassia.monteiro90@gmail.com>
2021-10-19 16:55:44 -0300

    cadastro, edicao e exclusao de cliente

---
+++ src/main/java/br/com/despachante/despachante_api/repository/ClienteRepository.java
@@ -0,0 +1,13 @@
+package br.com.despachante.despachante_api.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.despachante.despachante_api.modelo.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+	List<Cliente> findByNome(String nome);
+
+}
